aa52b3231ddaf519315d0ff50ad9bc82442cf889
VoR0220 <rj@erisindustries.com>
2016-12-02 17:45:00 -0600

    started unit tests for jobs
    Signed-off-by: VoR0220 <rj@erisindustries.com>

--- pkgs/jobs/jobs_unit_tests.go
+++ pkgs/jobs/jobs_unit_tests.go
@@ -1,10 +1,17 @@
 package jobs
 
-/*import (
+import (
 	"testing"
+	"os"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/eris-ltd/eris-cli/log"
+
+	"github.com/spf13/viper"
 )
 
-var testConfigFile string = `
+var testPreProcessingConfig string = `
 jobs:
 
 - name: setStorageBase
@@ -14,13 +21,13 @@ jobs:
 - name: deployStorageK
   deploy:
     contract: storage.sol
+    libraries: [$someName:$someAddress]
 
 - name: setStorage
   call:
     destination: $deployStorageK
     function: set
-    data:
-      - [1, 2, 3]
+    data: [$setStorage]
 
 - name: queryStorage
   query-contract:
@@ -40,6 +47,45 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
-func TestPreProcessingStrings(t *testing.T) {
+//copy of loaders for ease of testing
+func loadJobsForTesting(config string) (*Jobs, error){
+	log.Info("Loading Eris Run File...")
+	var jobset = EmptyJobs()
+	var epmJobs = viper.New()
+	
+	err := ioutil.WriteFile("epm.yaml", []byte(config), 0644)
+	defer os.Remove("epm.yaml")
+	if err != nil {
+		return nil, fmt.Errorf("cannot write config file: %v", err)
+	}
+	
+	epmJobs.AddConfigPath(".")
+	epmJobs.SetConfigName("epm")
 
-}*/
+	// load file
+	if err := epmJobs.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("Sorry, the marmots were unable to load the eris jobs file. Please check your path.\nERROR =>\t\t\t%v", err)
+	}
+
+	// marshall file
+	if err := epmJobs.Unmarshal(jobset); err != nil {
+		return nil, fmt.Errorf("Sorry, the marmots could not figure that eris jobs file out.\nPlease check your epm.yaml is properly formatted.\n")
+	}
+
+	return jobset, nil
+}
+
+func addJobResultsToMap(jobs *Jobs) (*Jobs, error) {
+	for i, job := range jobs {
+		job = job.PreProcess(jobs)
+		job, err = job.Execute(jobs)
+		jobs.JobMap[job.Name] = job.JobResults
+	}
+}
+
+func TestPreProcessingStrings(t *testing.T) {
+	jobs, err := loadJobsForTesting(testPreProcessingConfig)
+	if err != nil {
+		t.Fatalf("could not load jobs: %v", err)
+	}
+}
