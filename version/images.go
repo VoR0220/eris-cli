0224d3bdacd11ac3de72a452e0046653aee88358
RJ Catalano <rj@monax.io>
2017-05-02 07:47:16 -0500

    version
    Signed-off-by: RJ Catalano <rj@monax.io>

--- version/images.go
+++ version/images.go
@@ -11,7 +11,6 @@ const (
 	DB_VERSION        = "0.17.0"
 	KEYS_VERSION      = "0.17.0"
 	COMPILERS_VERSION = "0.17.0"
-	SOLC_VERSION      = "stable"
 )
 
 var (
@@ -22,5 +21,4 @@ var (
 	ImageDB        = fmt.Sprintf("monax/db:%s", DB_VERSION)
 	ImageKeys      = fmt.Sprintf("monax/keys:%s", KEYS_VERSION)
 	ImageCompilers = fmt.Sprintf("monax/compilers:%s", COMPILERS_VERSION)
-	ImageSolc      = fmt.Sprintf("ethereum/solc:%s", SOLC_VERSION)
 )
